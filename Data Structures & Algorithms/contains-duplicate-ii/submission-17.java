fe35637180d75e7c7d039537ca9598125d484885
jintang <slittywilly@gmail.com>
2026-05-21 21:45:21 +1000

    Add: contains-duplicate-ii - submission-17

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-17.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> dup = new HashSet<>();
+        for (int i = 0; i < k; i++) {
+            if (dup.contains(nums[i])) {
+                return true;
+            }
+            dup.add(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            
+            if (dup.contains(nums[i])) {
+                return true;
+            }
+            dup.remove(nums[i - k]);
+            dup.add(nums[i]);
+        }
+        return false;
+    }
+}
