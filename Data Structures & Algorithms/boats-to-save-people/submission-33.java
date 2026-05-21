4effa09885e2a8ced7f5675985960a606b57c2ed
jintang <slittywilly@gmail.com>
2026-05-21 19:04:14 +1000

    Add: boats-to-save-people - submission-33

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-33.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public int numRescueBoats(int[] people, int limit) {
+        int[] bucket = new int[limit + 1];
+        int counter = 0;
+        for (int num : people) {
+            bucket[num] ++;
+        }//1 2 2 3 3
+        int l = 0;
+        int r = bucket.length - 1;
+        while (l < r) { 
+            if (bucket[l] == 0) {
+                l++;
+                continue;
+            }
+            if (bucket[r] == 0) {
+                r--;
+                continue;
+            }
+            if (r == limit) {
+                System.out.println("f");
+                bucket[r] = bucket[r] - 1;
+                counter ++;
+                continue;
+            }
+            if (l + r > limit) {
+                System.out.println("f");
+                bucket[r] = bucket[r] - 1;
+                counter++;
+                continue;
+            }
+            System.out.println("f");
+            bucket[r] = bucket[r] - 1;
+            bucket[l] = bucket[l] - 1;
+            counter ++;
+        }
+        while (bucket[l] != 0) {
+            if (l * 2 <= limit && bucket[l] > 1) {
+                bucket[l] -= 2;
+                counter += 1;
+                continue;
+            }
+            bucket[l] = bucket[l] - 1;
+            counter++;
+        }
+        return counter;
+    }
+}
