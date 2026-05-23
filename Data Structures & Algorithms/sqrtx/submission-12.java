e0350a7a9a90feb5ebfff6693784169a4b6b78b2
jintang <slittywilly@gmail.com>
2026-05-23 17:20:40 +1000

    Add: sqrtx - submission-12

---
+++ Data Structures & Algorithms/sqrtx/submission-12.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        long l = 0;
+        long r = x;
+        while (l <= r) {
+            long mid = l + (r - l) / 2;
+            if (mid * mid == x) {
+                return (int) mid;
+            }
+            else if (mid * mid < x) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return (int) r;
+    }
+}
